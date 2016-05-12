0ffd1c1bad4baaf839c52298f8c9b8d0052aefb6
yannickv <yannick-verschueren1@hotmail.com>
2016-05-12 14:03:48 +0200

    First js

---
+++ app.js
@@ -0,0 +1,31 @@
+var foodquery = [
+ "SELECT ?subject1 ?name",
+ "WHERE {",
+ "?subject1 a <http://www.semanticweb.org/joliendeclerck/ontologies/2016/3/OIS-food-ontology/Ingredient>.",
+ "?subject1 <http://www.semanticweb.org/joliendeclerck/ontologies/2016/3/OIS-food-ontology/hasIngredientName> ?name.",
+ 'FILTER regex(?name, "Ch.","i").'
+].join(" ");
+
+var url = "http://localhost:3030/ds/query?query="
+
+function encodeQuery(query){
+ return url+"?query="+ encodeURIComponent(query)
+}
+
+
+function doQuery(query){
+
+	xhttp = new XMLHttpRequest();
+
+	xhttp.onreadystatechange= function() {
+		if(xhttp.readyState == 4 && xhttp.status == 200){
+			document.getElementById("test").innerHTML= xhttp.responseText;
+		}
+	}
+
+	xhttp.open("GET",query,true);
+	xhttp.send();
+
+
+
+
